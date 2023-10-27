7e5185c23ac9e822c7995a9285245e251d71d6c1
Jakub Cabala <jcabala@jcabala>
2023-10-27 14:44:05 +0100

    DS exercises

--- 6_Data_Structures_Exercises/main.py
+++ 6_Data_Structures_Exercises/main.py
@@ -42,7 +42,32 @@ if __name__ == "__main__":
     second_list = [4, 9, 16, 25, 36, 49, 64]
     print(set(zip(first_list, second_list)))
 
+    print("\nExercise 6")
     first_set = {23, 42, 65, 57, 78, 83, 29}
     second_set = {57, 83, 29, 67, 73, 43, 48}
     print(f"Intersection: {first_set.intersection(second_set)}")
     print(f"First minus second: {first_set.difference(second_set)}")
+
+    print("\nExercise 7")
+    first_set = {27, 43, 34}
+    second_set = {34, 93, 22, 27, 43, 53, 48}
+    
+    firstIsSubset = False
+    secondIsSubset = False
+
+    if first_set.issubset(second_set):
+        firstIsSubset = True
+    if second_set.issubset(first_set):
+        secondIsSubset = True
+
+    print(f"First set is subset of second set - {firstIsSubset}")
+
+    print(f"Second set is subset of First set - {secondIsSubset}")   
+
+    print("\nExercise 8")
+    roll_number = [47, 64, 69, 37, 76, 83, 95, 97]
+    sample_dict = {'Jhon':47, 'Emma':69, 'Kelly':76, 'Jason':97}
+    res = [val for val in roll_number if val in sample_dict.values()] 
+
+    print(roll_number)
+    print(f"After removing unwanted elements from list {res}")
